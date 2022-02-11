bbad72a6334cb4839768d8912c644b58744f8537
wanghao <wanghaobjyd@163.com>
2022-02-11 14:32:22 +0800

    update

--- excel/process.py
+++ excel/process.py
@@ -3,6 +3,9 @@ import cx_Oracle
 import yaml
 import os
 import threading
+import copy
+
+from openpyxl.chart import Reference
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -52,6 +55,7 @@ def sql_time(querysql, cur_year, cur_month):
 
     return querysql
 
+
 # 数据库拉取数据
 def ocrale_process(dbcursor, order_list, data_list):
     for i in range(len(order_list)):
@@ -71,8 +75,10 @@ def print_2dlist(list2d, str):
 
 # 获取日期
 def get_date(sheet):
-    cur_year = int(sheet.title[0:4])
-    cur_month = int(sheet.title[4:6])
+    year = int(sheet.title[0:4])
+    month = int(sheet.title[4:6])
+    cur_month = month + 1 if month < 12 else 1
+    cur_year = year if month < 12 else year + 1
     sta_year = cur_year if cur_month >= 6 else cur_year - 1
     sta_month = cur_month - 5 if cur_month >= 6 else (12 - 5 + cur_month)
 
@@ -106,6 +112,24 @@ def copy_rows(sheet, n, m):
     indexs = 'ABCD'  # 仅复制前4列
     for i in indexs:
         sheet[i + str(n)] = sheet[i + str(m)].value
+
+
+def get_datas_and_cats(sheet, row_table):
+    datas = []
+    cats = []
+    datas.append(Reference(worksheet=sheet, min_row=row_table[1]+1, max_row=row_table[1]+7, min_col=2, max_col=4))
+    cats.append(Reference(worksheet=sheet, min_row=row_table[1]+2, max_row=row_table[1]+7, min_col=1, max_col=1))
+
+    datas.append(Reference(worksheet=sheet, min_row=row_table[3]+1, max_row=row_table[3]+7, min_col=2, max_col=2))
+    cats.append(Reference(worksheet=sheet, min_row=row_table[3]+2, max_row=row_table[3]+7, min_col=1, max_col=1))
+
+    datas.append(Reference(worksheet=sheet, min_row=row_table[2]+1, max_row=row_table[2]+7, min_col=2, max_col=3))
+    cats.append(Reference(worksheet=sheet, min_row=row_table[2]+2, max_row=row_table[2]+7, min_col=1, max_col=1))
+
+    datas.append(Reference(worksheet=sheet, min_row=row_table[0]+1, max_row=row_table[0]+7, min_col=2, max_col=3))
+    cats.append(Reference(worksheet=sheet, min_row=row_table[0]+2, max_row=row_table[0]+7, min_col=1, max_col=1))
+
+    return datas, cats
 
 
 # 更新excel表格数据
@@ -146,8 +170,7 @@ def update_table(new_sheet, row_table, cur_year, cur_month, data_from_ocrale):
     print("休眠数据统计表格更新完成！！！！")
 
     # 更新表格4的数据
-    new_sheet['B' + str(row_table[3] + 7)] = new_sheet['H' + str(row_table[4] + 2)].value / new_sheet['H' + str(row_table[4] + 3)].value * 100
-    new_sheet['B' + str(row_table[3] + 7)].number_format = '##.00'
+    new_sheet['B' + str(row_table[3] + 7)] = round(new_sheet['H' + str(row_table[4] + 2)].value / new_sheet['H' + str(row_table[4] + 3)].value * 100, 2)
     print("表格4数据更新完成！！！！")
 
 
@@ -155,12 +178,14 @@ if __name__ == "__main__":
     cfg_data = read_config()
 
     excel = openpyxl.load_workbook(cfg_data["filepath"]["ori"])
-    new_sheet = excel[excel.sheetnames[-1]]
+    last_sheet = excel[excel.sheetnames[-1]]
+    new_sheet = copy.deepcopy(last_sheet)
+
+    cur_year, cur_month, sta_year, sta_month = get_date(new_sheet)  # 获取日期
+    new_sheet.title = str(cur_year) + str(cur_month).zfill(2) + "持仓数量按照客户号统计"
 
     row_table = get_row_table(new_sheet)  # 获取表格所在行
     print("5个表格所在行：", row_table, "\n")
-
-    cur_year, cur_month, sta_year, sta_month = get_date(new_sheet)  # 获取日期
 
     # 表格表头名称
     title1 = "中泰证券" + str(sta_year) + "年" + str(sta_month) + "月-" + str(cur_year) + "年" + str(cur_month) + "月"
@@ -174,37 +199,50 @@ if __name__ == "__main__":
 
     # 数据库数据
     data_from_ocrale = [[], [], [], []]
-    # dbhandle = cx_Oracle.connect('ql_read', 'ql_read', '10.29.180.151:2521/fzqsxt')
-    # dbcursor = dbhandle.cursor()
+    dbhandle = cx_Oracle.connect('ql_read', 'ql_read', '10.29.180.151:2521/fzqsxt')
+    dbcursor = dbhandle.cursor()
 
-    # threads = []
-    # threads.append(threading.Thread(target=ocrale_process,args=(dbcursor, querysql[0], data_from_ocrale[0])))
-    # threads.append(threading.Thread(target=ocrale_process,args=(dbcursor, querysql[1], data_from_ocrale[1])))
-    # threads.append(threading.Thread(target=ocrale_process,args=(dbcursor, querysql[2], data_from_ocrale[2])))
-    # threads.append(threading.Thread(target=ocrale_process,args=(dbcursor, querysql[3], data_from_ocrale[3])))
+    threads = []
+    threads.append(threading.Thread(target=ocrale_process, args=(dbcursor, querysql[0], data_from_ocrale[0])))
+    threads.append(threading.Thread(target=ocrale_process, args=(dbcursor, querysql[1], data_from_ocrale[1])))
+    threads.append(threading.Thread(target=ocrale_process, args=(dbcursor, querysql[2], data_from_ocrale[2])))
+    threads.append(threading.Thread(target=ocrale_process, args=(dbcursor, querysql[3], data_from_ocrale[3])))
 
     # 启动所有线程
-    # for t in threads:
-    #     # t.setDaemon(True)
-    #     t.start()
+    for t in threads:
+        # t.setDaemon(True)
+        t.start()
     # 主线程中等待所有子线程退出
-    # for thread in threads:
-    #     thread.join()
+    for thread in threads:
+        thread.join()
 
-    data_from_ocrale[0].append(57237)
-    data_from_ocrale[0].append(52560)
-    data_from_ocrale[1].append(2021)
-    data_from_ocrale[1].append(3284)
-    data_from_ocrale[1].append(2917)
-    data_from_ocrale[2].append(7603607)
-    data_from_ocrale[2].append(6215629)
-    data_from_ocrale[3].append(1870248)
-    data_from_ocrale[3].append(4565310)
-    data_from_ocrale[3].append(6509292)
-
-    print_2dlist(data_from_ocrale, "数据库获取的数据：")  # 打印数据库数据
+    # 打印数据库数据
+    print_2dlist(data_from_ocrale, "数据库获取的数据：")
 
     update_table(new_sheet, row_table, cur_year, cur_month, data_from_ocrale)  # 更新表格数据
+
+    # modify charts
+    mark = cfg_data["mark"]["mark"]
+    color = cfg_data["mark"]["color"]
+
+    charts = []
+    datas, cats = get_datas_and_cats(new_sheet, row_table)
+
+    for chart in new_sheet._charts:
+        chart.ser = []
+        charts.append(chart)
+
+    for i in range(len(charts)):
+        charts[i].add_data(datas[i], titles_from_data=True)
+        charts[i].set_categories(cats[i])
+        for j in range(len(charts[i].ser)):
+            charts[i].ser[j].marker = openpyxl.chart.marker.Marker(symbol=mark[j], size=7)
+            charts[i].ser[j].graphicalProperties.line.width = 30000
+            charts[i].ser[j].graphicalProperties.line.solidFill = color[j]
+            charts[i].ser[j].marker.graphicalProperties.solidFill = color[j]
+        charts[i].dLbls.showVal = True
+
+    excel._sheets.append(new_sheet)
 
     print("done!!!!!")
 
